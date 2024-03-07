812bd10fae984610caada50754ad04d0dc5a77b6
Romeo Iordachescu <romeo.iordachescu@gmail.com>
2024-03-07 22:25:46 +0100

    Caricata la Traccia 1 e Traccia 2

---
+++ index02.php
@@ -0,0 +1,91 @@
+<?php
+
+// Traccia 2 
+
+class Vertebrates {
+        protected static function vert(){
+            echo "Sono un animale Vertebrato" . "\n";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+    }
+}
+
+class WarmBlooded extends Vertebrates {
+        protected static function warm(){
+            echo "Sono un animale a Sangue Caldo" . "\n";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            WarmBlooded::warm();
+    }
+}
+
+class ColdBlooded extends Vertebrates {
+        protected static function cold(){
+            echo "Sono un animale a Sangue Freddo" . "\n";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            ColdBlooded::cold();
+    }
+}
+
+class Mammals extends WarmBlooded {
+        protected static function mamm(){
+            echo "Sono un mamifere"."\n";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            WarmBlooded::warm();
+            Mammals::mamm();
+    }
+}
+
+class Birds extends WarmBlooded {
+        protected static function bird(){
+            echo "Sono un uccello";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            WarmBlooded::warm();
+            Birds::bird();
+    }
+}
+
+class Fish extends ColdBlooded {
+        protected static function fish(){
+            echo "Sono un pesce";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            ColdBlooded::cold();
+            Fish::fish();
+    }
+}  
+
+class Reptiles extends ColdBlooded {
+        protected static function rept(){
+            echo "Sono un rettile";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            ColdBlooded::cold();
+            Reptiles::rept();
+    }
+}
+
+class Amphibians extends ColdBlooded  {
+        protected static function amphi(){
+            echo "Sono un anfibia";
+        }
+        public function __construct(){
+            Vertebrates::vert();
+            ColdBlooded::cold();
+            Amphibians::amphi();
+    }
+}
+
+$magikarp = new Fish();
+
+?>
